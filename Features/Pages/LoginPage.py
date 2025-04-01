abaed1efcd01437b223ff12a317b71216519fa76
shreya <shreyapradhan81@gmail.com>
2025-04-02 00:07:13 +0530

    pom changes

--- Features/Pages/LoginPage.py
+++ Features/Pages/LoginPage.py
@@ -3,8 +3,17 @@ class LoginPage:
         self.driver=driver
 
     login_button_xpath = '//button[@type="submit"]'
+    username_xpath = '//input[@name="username"]'
+    password_xpath = '//input[@type="password"]'
 
-    def click_on(self):
+    def click_on_login(self):
         self.driver.find_element('xpath', self.login_button_xpath).click()
+
+    def enter_username(self, username_text):
+        self.driver.find_element('xpath', self.username_xpath).send_keys(username_text)
+
+    def enter_password(self, password_text):
+        self.driver.find_element('xpath', self.password_xpath).send_keys(password_text)
+
 
 
--- Features/Steps/login.py
+++ Features/Steps/login.py
@@ -7,24 +7,30 @@ from Features.Pages.LoginPage import LoginPage
 
 @given(u'I got navigated to OrangeHRM')
 def step_impl(context):
+    context.login_page = LoginPage(context.driver)
     assert context.driver.find_element('xpath', '//h5[text()="Login"]').text == 'Login'
 
 
 @when(u'I enter username')
 def step_impl(context):
-    context.driver.find_element('xpath', '//input[@name="username"]').send_keys('Admin')
+    context.login_page.enter_username('Admin')
+
+    # context.driver.find_element('xpath', '//input[@name="username"]').send_keys('Admin')
 
 
 @when(u'I enter password')
 def step_impl(context):
-    context.driver.find_element('xpath', '//input[@type="password"]').send_keys('admin123')
+    context.login_page.enter_password('admin123')
+
+    # context.driver.find_element('xpath', '//input[@type="password"]').send_keys('admin123')
 
 
 @when(u'Click on login')
 def step_impl(context):
-    login_page = LoginPage(context.driver)
-    login_page.login_button_xpath()
-    #context.driver.find_element('xpath', '//button[@type="submit"]').click()
+    context.login_page.click_on_login()
+
+    # context.driver.find_element('xpath', '//button[@type="submit"]').click()
+
 
 @then(u'User should land onto Homepage')
 def step_impl(context):
@@ -33,12 +39,15 @@ def step_impl(context):
 
 @when(u'I enter \'adm\'')
 def step_impl(context):
-    context.driver.find_element('xpath', '//input[@name="username"]').send_keys('adm')
+    context.login_page.enter_username('adm')
+
+    # context.driver.find_element('xpath', '//input[@name="username"]').send_keys('adm')
 
 
 @when(u'I enter \'12\'')
 def step_impl(context):
-    context.driver.find_element('xpath', '//input[@type="password"]').send_keys('23')
+    context.login_page.enter_password('23')
+    # context.driver.find_element('xpath', '//input[@type="password"]').send_keys('23')
 
 
 @then(u'User should see "Invalid credentials"')
